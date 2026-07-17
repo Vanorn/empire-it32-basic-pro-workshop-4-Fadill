5770d9fbf60bfb39f1787b91ca123a37ae5b0c51
Student <Student@Train11-2-PC361.control.ad.sit.kmutt.ac.th>
2026-07-17 12:44:59 +0700

    youngmaiset

---
+++ workshop4.py
@@ -0,0 +1,19 @@
+gang_members = []
+
+def add_members(name , age , type):
+    print(add_members)
+    gang_members.append(new_member)
+
+
+while True:
+    answer = int(input("จะทำอะไร"))
+    if answer == 1 :
+        name = input("Enter your name :")
+        age = int(input("Enter your age: :"))
+        gang = str(input("Enter your gang :"))
+        new_member = {name , age ,gang}
+        add_members(name , age , gang)
+    else :
+        break
+print(gang_members)
+    
